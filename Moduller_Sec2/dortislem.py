ee5212d37d3e189b0934a90c953c052ab5fab5c5
Gözde Mihran Altınsoy <32915384+gmihran@users.noreply.github.com>
2019-05-02 15:56:34 +0300

    Add files via upload

---
+++ Moduller_Sec2/dortislem.py
@@ -0,0 +1,23 @@
+"""
+Soru 2. İki parametre alan toplama(s1,s2), çıkarma(s1,s2), çarpma(s1,s2) ve bölme(s1,s2)  fonksiyonlarına sahip (toplama, çıkarma, çarpma, bölme işlemlerini hesaplatan) dortislem isimli modül oluşturalım ve projemize import edip fonksiyonları test edelim
+"""
+def toplama(s1,s2):
+    """
+    İki sayının toplamını döndürür
+    """
+    return s1+s2
+def cikarma(s1,s2):
+    """
+    İki sayının farkını döndürür
+    """
+    return s1-s2  
+def carpma(s1,s2):
+    """
+    İki sayının çarpımını döndürür
+    """
+    return s1*s2  
+def bolme(s1,s2):
+    """
+    İki sayının bölümünü döndürür
+    """
+    return s1/s2  
---
+++ Moduller_Sec2/modulum.py
@@ -0,0 +1,16 @@
+"""
+İki sayının toplamını ve üç sayının toplamını hesaplayan fonksiyonlar barındırır
+"""
+
+liste=[5,3,2,1,10,7]
+
+def toplam2(s1,s2):
+    """
+    İki sayının toplamını hesaplar
+    """
+    return s1+s2
+def toplam3(s1,s2,s3):
+    """
+    Üç sayının toplamını hesaplar
+    """
+    return s1+s2+s3
